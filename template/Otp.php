cdf9c9094f3320a280fa0ec65c852a4c165f3ed6
bekawa <www.bereketkassahun12@gmail.com>
2023-11-13 02:04:08 +0300

    commit

--- template/Otp.php
+++ template/Otp.php
@@ -2,8 +2,7 @@
 <html lang="en">
 
 <head>
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-  <!-- Required meta tags -->
+
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
